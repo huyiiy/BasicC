2b93d21218c12f642c8f07b7ab44d2f13727b60e
HU <1728995476@qq.com>
2023-12-08 20:35:32 +0800

    [修改]修改了动静态数组和字符数组

--- BasicC/demoArray/demoArrayInvalidAccess.c
+++ BasicC/demoArray/demoArrayInvalidAccess.c
@@ -2,52 +2,33 @@
 
 
 #define BUFFER_SIZE 10
-int main ()
+
+int main()
 {
-
 #if 0
-    /* 如何获取数组有多少个元素： 数组的总长度/每个元素的长度 */
-    long array[BUFFER_SIZE] = { 0 };
+    int array[BUFFER_SIZE] = { 0 };
 
-    int len = sizeof(array);
-    printf("len:%d\n", arrayLen);
-
-    int elementLen = sizeof(array[0]);
-    printf("len:%d\n", elementLen);
-
-    int length = arrayLen / elementLen;
-    printf("length:%d\n", length);
+    /* 数组的越界访问 */
+    /* 判断索引的有效性 ： 索引必须是合理的范围RANGE [0 - BUFFER_SIZE-1] */
+    printf("array[-1]:%d\n",  array[-1]);
+    printf("array[10]:%d\n",  array[10]);
 #endif
 
-#if 0
-   /* 字符数组 */
-   /* Q1 : 什么叫字符？*/
-   char pNum = 'a';
-   printf("pNum:%c\n", pNum);
+#if 1
+   /* 静态数组 */
+   int desk[BUFFER_SIZE] = { 0 };
 
-   /* 只要是指针的变量，变量定义第一个需要是p开头 */
-   char * ptr = "hello world!";
-   printf("ptr:%s\n", ptr);
+   /* 动态数组 */
+   /* 算法 ：
+        自动扩容
+        自动缩容
+   */
 
-   char buffer[] = {'h', 'e', 'l', 'l', 'o','w', 'o', 'r', 'l', 'd'};
-   int buflen = sizeof(buffer);
-   printf("buflen:%s\n", buflen);
 #endif
-   
-   /* 字符数组初始化定义的两种方式 */
-   char buffer1[] = "helloworld";
-   /* 如果是以双引号（字符串）的方式 默认加上'\0'*/
-   char buffer2[] = {'h', 'e', 'l', 'l', 'o','w', 'o', 'r', 'l', 'd'};
-   char buffer3[] = {'h', 'e', 'l', 'l', 'o','w', 'o', 'r', 'l', 'd'};
-   /* 如果是以单引号（字符）的方式 默认不加上'\0'*/
-   int buflen1 = sizeof(buffer1);
-   int buflen2 = sizeof(buffer2);
 
-    printf("buflen1:%p\n", buffer1);
-    printf("buflen2:%p\n ", buffer2);
-    printf("buflen3:%p\n ", buffer3);
+#if 1
+   /* 字符数组 <---> 字符串 */
 
-    printf("buflen1 = %d,\tbuflen2 = %d\n",buflen1,buflen2);
-    printf("buffer1 = %s,\tbuffer2 = %s\n",buffer1,buffer2);
+#endif
     return 0;
 }
