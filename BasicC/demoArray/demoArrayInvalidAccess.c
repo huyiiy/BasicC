2fcbca6ad057a7b82bbc33429bb6902a8b41cd4c
HU <1728995476@qq.com>
2023-12-08 21:00:13 +0800

    [修改]修改了静态数组部分

--- BasicC/demoArray/demoArrayInvalidAccess.c
+++ BasicC/demoArray/demoArrayInvalidAccess.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
 
-
 #define BUFFER_SIZE 10
 
 int main()
-{
+{   
+#if 0   //静态数组
+    int array[BUFFER_SIZE]={ 0 };
+    printf("array[-1]:%d\n",array[-1]);
+    printf("array[10]:%d\n",array[10]);
+#endif 
+
 #if 0
-    int array[BUFFER_SIZE] = { 0 };
-
-    /* 数组的越界访问 */
-    /* 判断索引的有效性 ： 索引必须是合理的范围RANGE [0 - BUFFER_SIZE-1] */
-    printf("array[-1]:%d\n",  array[-1]);
-    printf("array[10]:%d\n",  array[10]);
+int array[BUFFER_SIZE]={ 0 };
+    //动态数组
+    //算法：自动扩容
 #endif
-
 #if 1
-   /* 静态数组 */
-   int array[BUFFER_SIZE] = { 0 };
-   printf("array[-1]:%d\n",array[-1]);
-   printf("array[1o]:%d\n",array[10]);
-   /* 动态数组 */
-   /* 算法 ：
-        自动扩容
-        自动缩容
-   */
+
+
 
 #endif
+    return 0;
 
-#if 1
-   /* 字符数组 <---> 字符串 */
-
-#endif
-    return 0;
 }
